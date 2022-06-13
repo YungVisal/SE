319f77a0a5991e62ccedfa542659130d4d0e34f7
Yung Visal <yungvisal@gmail.com>
2022-06-13 15:15:56 +0700

    FinalExam

---
+++ library/src/test/java/edu/itci4gic/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itci4gic.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
